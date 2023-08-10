bb0cd103d8ca77ac63e791ebada8878570d620dd
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-10 16:39:22 -0700

    adding new files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>;
+/*
+ * main - Entry point
+ *
+ * Description : 'function that pring statement with printf'
+ *
+ * Return : Alawys 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
