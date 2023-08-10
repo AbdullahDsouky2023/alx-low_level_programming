a3cab5212fd5baff3ac7e053d5d560e6ab795a6d
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-10 15:50:59 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int main(void) {
+/*thiis function is used to creating a statemetnt */
+int main(void)
+{
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
