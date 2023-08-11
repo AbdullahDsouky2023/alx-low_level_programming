dd229434a78d9997a4976618a27939c6e4d579b0
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-10 18:21:31 -0700

    adding new files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+  main - Entry point
+
+  Description :'function the unix system that print using write'
+
+  Return :Alawys 0 (Success)
+*/
+int main(void)
+{
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(1, message, sizeof(message) - 1);  
+    return 1;
+}
