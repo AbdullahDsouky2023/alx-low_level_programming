010801f13d14ece6d01d03aed9e8f407ab284957
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-11 16:21:49 -0700

    adding new files

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+  *  main - Entry point
+  *
+  *  Description: 'the program's description'
+  *
+  *  Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+	int count = 10;
+	int i = 0;
+	
+	while (i < count )
+	{
+		putchar(hexDigits[i]);
+		if(i == 9)
+			putchar('$');
+		else
+		{
+			putchar(' ,');
+		}
+		i++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
