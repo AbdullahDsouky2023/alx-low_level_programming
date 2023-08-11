27ddae8ae7c1992134b54cd79c7641b1d8a63e71
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-10 18:30:05 -0700

    adding new files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <unistd.h>
 /**
-  main - Entry point
-
-  Description :'function the unix system that print using write'
-
-  Return :Alawys 0 (Success)
+  *  main - Entry point
+  *
+  *  Description: 'the program's description'
+  *
+  *  Return: Always 0 (Success)
 */
+
 int main(void)
 {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(1, message, sizeof(message) - 1);  
-    return 1;
+	char message[] = "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19\n";
+	write(1, message, sizeof(message) - 1);
+	return (1);
 }
