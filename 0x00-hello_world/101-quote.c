f2b36773412d6746f39f5d897e57109060c66e6f
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-10 19:06:30 -0700

    adding new files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,11 @@
   *  Return: Always 0 (Success)
 */
 
+
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char message[] = "and that piece of "
+		"art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(1, message, sizeof(message) - 1);
 	return (1);
 }
