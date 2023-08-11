227702adea475ef6a1c084d3794019952477b4e1
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-11 13:19:57 -0700

    adding new files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *
+ *   main - Entry point
+ *
+ *   Decription : 'function for priing the alphabet'
+ *
+ *   Return :0 (Success)
+*/
+
+int main(void)
+{
+	char ch='a';
+	while(ch < 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+
+}
