f31b87073f3d69735996af25c68db0b0cd471731
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-11 14:43:07 -0700

    adding new files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ /*
+  *  main - Entry Point
+  *
+  *  Description :' function pring number'
+  *
+  *  Return : 0 (Success)
+ */
+int main(void)
+{
+	for (int i=0; i<10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
