40997c48054950ecb87c1ecdfc28b90a7f4485b6
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-10 17:05:12 -0700

    adding new files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@
 
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\n");
-	printf("Size of a int: 1 byte(s)\n");
-	printf("Size of a long int: 1 byte(s)\n");
-	printf("Size of a long long int: 1 byte(s)\n");
-	printf("Size of a float: 1 byte(s)\n");
+	printf("Size of a char: %zu  byte(s)\n",sizeof(char));
+	printf("Size of a int: %zu byte(s)\n",sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n",sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n",sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n",sizeof(float));
 	return (0);
 }
