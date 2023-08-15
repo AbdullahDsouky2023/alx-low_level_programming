2776c01dca1bd86474a10e1eb3ef5a4b581b4440
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-15 15:34:15 -0700

    adding new files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- *  main - Entry point
- * 
+ * print_alphabet - Entry point
+ *
  * Description :'function that uses put char in universe '
- * 
- * Return :Always 0 success
+ *
+ * Return : void 1
 */
 
-void print_alphabet(void){
-  char str='a';
+void print_alphabet(void)
+{
+	char str = 'a';
 
-  while(str<='z'){
-    _putchar(str);
-    str++;
-  }
-  _putchar('\n');
+	while (str <= 'z')
+	{
+		_putchar(str);
+		str++;
+	}
+	_putchar('\n');
 }
