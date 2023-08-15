e60fc89246e09b8d6d7286471558f6e71f19e1c9
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-15 15:18:15 -0700

    adding new files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,7 +8,13 @@
  * Return :Always 0 success
 */
 
-int main(void){
-  print_alphabet();
+int print_alphabet(void){
+  char str='a';
+  int i=0;
+  while(str<='z'){
+    _putchar(str);
+    str++;
+  }
+  _putchar('\n');
   return 0;
 }
