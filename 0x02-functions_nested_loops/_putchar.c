768ab0febcd033148486fc7bc06f53a261eb1958
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-15 14:45:37 -0700

    adding new files

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "main.h"
 /**
  * _putchar - writes the character c to stdout
@@ -6,13 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
-int main(void){
-  char str[10]="_putchar";
-  int i=0;
-  while(i<=7){
-    _putchar(str[i]);
-    i++;
-  }
-  return 0;
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
