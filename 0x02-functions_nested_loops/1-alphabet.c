236d41b098e1a953999e07fdeb1226c0932a4177
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-15 15:13:05 -0700

    adding new files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  main - Entry point
+ * 
+ * Description :'function that uses put char in universe '
+ * 
+ * Return :Always 0 success
+*/
+
+int main(void){
+  print_alphabet();
+  return 0;
+}
---
+++ 0x02-functions_nested_loops/_alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  main - Entry point
+ * 
+ * Description :'function that uses put char in universe '
+ * 
+ * Return :Always 0 success
+*/
+
+int print_alphabet(void){
+  char str='a';
+  int i=0;
+  while(str<='z'){
+    _putchar(str);
+    str++;
+  }
+  _putchar('\n');
+  return 0;
+}
