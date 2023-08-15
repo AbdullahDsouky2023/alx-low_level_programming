98d6e138187ec5ef8c20c58b85b6d9db68400d5b
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-14 21:32:04 -0700

    adding new files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ *
+ * main - Entry point
+ *
+ * Description :"function for pringing statement"
+ *
+ * Return : Alawys0 (Success)
+*/
+
+int main(void)
+{
+	printf("0-putchar.c");
+	return (0);
+}
