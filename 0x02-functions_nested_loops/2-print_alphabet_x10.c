499fa8de134fd6579be66375ca9c14117387c14f
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-15 17:04:29 -0700

    adding new files

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,30 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - Entry point
- * 
+ *
  * Description :'function that uses put char in universe '
- * 
+ *
  * Return :Always 0 success
 */
 
 void print_alphabet_x10(void)
 {
-  int counter=0;
-  while(counter<=9){
-  char str='a';
-  while(str <='z')
-  {
-    _putchar(str);
-    str++;
-  }
-    _putchar('\n');
-  if(str=='z'){
-    str ='a';
-  }
-  counter++;
- 
-  }
+	int counter = 0;
+
+	while (counter <= 9)
+	{
+		char str = 'a';
+
+		while (str <= 'z')
+		{
+			_putchar(str);
+			str++;
+		}
+		_putchar('\n');
+		if (str == 'z')
+		{
+			str = 'a';
+		}
+		counter++;
+	}
 }
