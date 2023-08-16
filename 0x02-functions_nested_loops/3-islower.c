475e0074bca5b4e6ea72468c2fa10ec19e552bd7
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-15 17:27:01 -0700

    adding new files

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,21 +2,22 @@
 #include <ctype.h>
 
 /**
- * print_alphabet_x10 - Entry point
- * 
- * Description :'function that uses put char in universe '
- * 
- * Return :Always 0 success
+ * _islower - Checks if a character is lowercase.
+ *
+ * @c: The character to be checked.
+ *
+ * Return: 1 if the character is lowercase, 0 otherwise.
 */
 
 int _islower(int c)
 {
-  if (islower(c) )
-  {
-    return (1);
-  } else if (!islower(c))
-  {
-    return (0);
-  }
-  return 0;
+	if (islower(c))
+	{
+		return (1);
+	}
+	else if (!islower(c))
+	{
+		return (0);
+	}
+	return (0);
 }
