3af34fab078e9accd2dfa25d3f318cb80b3e5d45
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-15 18:11:47 -0700

    adding new files

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - print_last_digit
+ *
+ * @x: The character to be checked.
+ *
+ * Return:return last digit .
+ */
+int print_last_digit(int x)
+{
+	return (printf("%d",x %10));
+}
