d151112360f0046181a4853597f031fad27ce438
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-15 19:44:59 -0700

    adding new files

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ void print_times_table(int n)
 			_putchar (48);
 			for (mult = 1; mult <= n; ++mult)
 			{
-				_putchar(','1);
+				_putchar(',');
 				_putchar(' ');
 				prod = num * mult;
 				if (prod <= 9)
