00c2b74f0e794b33dc107e49aee5f2455b2ff769
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-15 17:45:53 -0700

    adding new files

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_sign - Checks if + or - or zero.
+ *
+ * @n: The character to be checked.
+ *
+ * Return: 1 if the character is +,-1 is - 0 otherwise.
+ */
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
