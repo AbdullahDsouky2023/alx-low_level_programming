2f7fe770ad73824348d59a6ec0fabaca1b57faa1
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-15 18:55:10 -0700

    adding new files

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,9 +2,10 @@
 /**
 * jack_bauer- prints every minute of the day
 */
-void jack_bauer (void)
+void jack_bauer(void)
 {
 	int min, hr;
+
 	for (hr = 0; hr <= 23; hr++)
 	{
 		for (min = 0; min <= 59; min++)
