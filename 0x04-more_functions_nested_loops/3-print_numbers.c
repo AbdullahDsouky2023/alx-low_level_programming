cf17f29cba755cd6c7c54439a5b67b00beaf32d2
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-17 15:40:11 -0700

    adding new files

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_numbers- checks is it is uppercase4 or not 
+*/
+
+void print_numbers(void)
+{
+  for(int i=48;i<57;i++)
+    {
+    putchar(i);
+    }
+  putchar('\n');
+}
