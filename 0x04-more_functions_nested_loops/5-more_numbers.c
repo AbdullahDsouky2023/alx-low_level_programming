65674f1e2d739097b772f1da3de1b448839e6de8
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-17 16:30:59 -0700

    adding new files

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* more_numbers - checks is it is uppercase4 or not 
+*
+* @c :the char to be printed
+*/
+
+
+
+void more_numbers(void)
+{
+  int k=0;
+  while(k <10){
+  int i=0;
+  while(i<=14)
+    {
+      if(i > 9 )
+      {
+         _putchar((i/10) +48);
+      }
+      _putchar((i%10) +48);
+      i++;
+    }
+  _putchar('\n');
+    k++;
+  }
+}
