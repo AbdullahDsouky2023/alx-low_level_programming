e5a5681a1635d371fa152b507f513beb0d766534
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-17 15:29:58 -0700

    adding new files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* mul- checks is it is uppercase4 or not 
+* 
+* @a :the the digit to be checked 
+* @b :the the digit to be checked 
+* 
+* Return: 0
+*/
+
+int mul(int a ,int b)
+{
+    return (a * b );
+}
