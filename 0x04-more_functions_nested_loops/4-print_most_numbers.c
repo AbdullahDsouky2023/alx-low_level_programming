792043b31fa520d22a023dc7d61b2a27b6f01692
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-17 15:59:43 -0700

    adding new files

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@
 
 void print_most_numbers(void)
 {
-        int i = 48;
+	int i = 48;
 
-        while (i <= 57)
-        {
-		if(i != 50 && i != 52)
+	while (i <= 57)
+	{
+		if (i != 50 && i != 52)
 		{
 			_putchar(i);
 		}
