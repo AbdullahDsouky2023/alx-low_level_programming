8749f5822b95563a49532adbcf7c6122f6c1012d
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-17 19:09:02 -0700

    adding new files

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,3 +1,21 @@
+
+/*
+_sqrt - finds the square root
+@x: input number
+* Return: square root of x
+*/
+double _sqrt(double x)
+{
+float sqrt, tmp;
+sqrt = x / 2;
+tmp = 0;
+while (sqrt != tmp)
+{
+tmp = sqrt;
+sqrt = (x/tmp + tmp) / 2;
+}
+return (sqrt);
+}
 void largest prime_factor (long int num)
 {
 int prmNu, largest;
