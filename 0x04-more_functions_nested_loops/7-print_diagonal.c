ba6bf280759ad43ce0266fc71e65943a9ffddddb
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-17 17:08:49 -0700

    adding new files

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,21 +1,23 @@
 #include "main.h"
 /**
-*  print_diagonal - print diagona 
-*
+*  print_diagonal - print diagona
 * @n :n of /
 */
 void print_diagonal(int n)
 {
-  int k=0;
-  while(k <n){
-   int i =0;
-    while(i < k){ 
-  _putchar(' ');
-      i++;
-    }
-  _putchar('\\');
-    
-  _putchar('\n');
-    k++;
-  }
+	int k = 0;
+
+	while (k < n)
+	{
+		int i = 0;
+
+		while (i < k)
+		{
+			_putchar(' ');
+			i++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		k++;
+	}
 }
