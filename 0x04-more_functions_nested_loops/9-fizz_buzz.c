fe0b2875937e84cc6461c9aa281cd7f1d7ea71b9
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-17 18:10:16 -0700

    adding new files

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
+ * Descritpion :' Function that print putchar char'
  *
+ * Return: On success 0
 */
 int main(void)
 {
-  int i = 1;
+	int i = 1;
 
-  while(i <100)
-    {
-       if ((i % 3 == 0  && i % 5 == 0)  )
-      {
-      printf("FizzBuzz\n");
-
-      }else if(i % 3 == 0)
-      {
-      printf("Fizz\n");
-      }
-      else if(i % 5 == 0)
-      {
-      printf("Buzz\n");
-      }
-      else
-      {
-      printf("%d\n",i);
-      }
-      i++;
-    }
-  return (0);
+	while (i < 100)
+	{
+		if ((i % 3 == 0  && i % 5 == 0))
+		{
+			printf("FizzBuzz\n");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
+		i++;
+	}
+	return (0);
 }
