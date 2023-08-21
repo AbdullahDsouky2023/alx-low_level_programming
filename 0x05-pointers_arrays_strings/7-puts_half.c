b9956952a7e2a046a7036f0db057ae4aa70f1363
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-21 12:42:23 -0700

    adding new files

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - function
+ * @str : the char to know
+*/
+void puts_half(char *str)
+{
+        int le = strlen(str) - 1;
+	int i = le / 2 ;
+
+	while (i <= le)
+        {
+                putchar(str[i]);
+                i++;
+        }
+        putchar('\n');
+}
