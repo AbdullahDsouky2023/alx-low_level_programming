b334dad451d8f39570d3e86b6c0322861dfd13b6
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-21 02:20:32 -0700

    adding new files

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function
+ * @s :the char to know it;s length
+ * Return : the length of char
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
