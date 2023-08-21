f0583209acca831b2548e7f1ef6116deb2757ca6
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-21 02:38:14 -0700

    adding new files

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
- *
+ * _puts -  function to print word
+ * @str : the word to be printed
+ * Description: 'function desc'
  * Return: Always 0.
  */
 
 void _puts(char *str)
 {
-        puts(str);
+	puts(str);
 }
