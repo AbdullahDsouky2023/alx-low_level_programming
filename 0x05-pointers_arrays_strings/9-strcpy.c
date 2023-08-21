905d3ff58f77193ead2cec601161518352e4412f
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-21 13:38:45 -0700

    adding new files

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,9 +2,10 @@
 #include <string.h>
 #include "main.h"
 /**
- * *_strcpy - function
+ * _strcpy - function
  * @dest : the char to know
  * @src : the char to know
+ * Return: the returned char
 */
 char *_strcpy(char *dest, char *src)
 {
