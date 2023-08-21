06d141347e53d236ebf119a0ef95ae19c7f8473a
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-21 12:38:00 -0700

    adding new files

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,12 +3,12 @@
 #include "main.h"
 /**
  * puts2 - function
- *
- * /
+ * @str : the char to know
+*/
 void puts2(char *str)
 {
 	int i = 0;
-	int le = strlen(str) -1;
+	int le = strlen(str) - 1;
 
 	while (i <= le)
 	{
