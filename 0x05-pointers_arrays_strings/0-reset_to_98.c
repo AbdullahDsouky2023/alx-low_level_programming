60bce9b4147e000a8f99e8164b21bc94ac77d5ac
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-21 01:57:45 -0700

    adding new files

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- *  reset_to_98 - function t takes a pointer to an int as parameter and updates the value it points to to 98
- *  @p : the pointer the int to be edit 
+ *  reset_to_98 - function t takes a pointer
+ *  to an int as parameter and updates
+ *  the value it points to to 98
+*  *  @n : the pointer the int to be edit
 */
 void reset_to_98(int *n)
 {
