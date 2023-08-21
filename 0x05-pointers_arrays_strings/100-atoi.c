0f460793f1e900de3cfbf198b3d0e6bea0de8db9
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-21 13:46:47 -0700

    adding new files

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *_strcpy - function
+ * @s : the char to know
+ * Return: string to int
+*/
+int _atoi(char *s)
+{
+      return (atoi(s));
+}
