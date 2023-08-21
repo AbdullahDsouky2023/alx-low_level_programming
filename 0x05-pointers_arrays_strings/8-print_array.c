2924969480cea310981a7799b8127745abe7dd62
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-21 13:13:49 -0700

    adding new files

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,7 @@
 {
        int i = 0;
        while(i < n){
-          printf("%d",a[i]);
-           putchar(',');
-           putchar(' ');
+          printf("%d ,",a[i]);
            i++;
        }
 }
