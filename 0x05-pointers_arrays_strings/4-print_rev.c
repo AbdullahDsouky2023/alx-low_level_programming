698a95429be8ed8232fa636f825ff872b5894de4
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-21 04:56:31 -0700

    adding new files

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  *  print_rev - check the code
  *  @s: para to be used
@@ -6,11 +7,12 @@
 
 void  print_rev(char *s)
 {
-        int len = _strlen(s);
+        int len = strlen(s);
 
         while(len > 0)
         {
                 _putchar(s[len]);
+		len--;
         }
 
         _putchar('\n');
