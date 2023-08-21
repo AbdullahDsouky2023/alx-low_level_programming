3c3e7db32c296d86c4f6caf1d930439f9356a3f8
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-21 13:12:32 -0700

    adding new files

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_array - function
+ * @a : the char to know
+ * @b : the char to know
+*/
+ void print_array(int *a, int n)
+{
+       int i = 0;
+       while(i < n){
+          printf("%d",a[i]);
+           putchar(',');
+           putchar(' ');
+           i++;
+       }
+}
