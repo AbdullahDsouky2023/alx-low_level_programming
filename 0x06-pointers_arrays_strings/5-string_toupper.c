8179c796d3489d19a60a66f304229e5a1bbc5069
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-24 06:39:49 -0700

    adding new files

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * string_toupper  - Concatenates two strings.
+ * @str: The destination string.
+ *
+ * Return: Pointer to the concatenated string.
+ */
+
+char *string_toupper(char *str)
+{
+    int i =0 ;
+    while(str[i] != '\0')
+    {
+        if(str[i] >= 97){
+        str[i]=str[i]-32;
+
+        }else {
+
+                str[i]=str[i];
+
+        }
+
+        i++;
+    }
+  return str;
+}
