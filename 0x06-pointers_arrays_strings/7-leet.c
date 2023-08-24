fdec2545898fe672a7526c8ca193c4f91c1b50f0
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-24 12:20:28 -0700

    adding new files

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,33 +10,24 @@
  *
  * Return: A pointer to the modified input string.
  */
-char *leet(char *s) {
-    int i = 0;
-    char re[5] = {'a', 'e', 'o', 't', 'l'};
-    char re2[5] = {'A', 'E', 'O', 'T', 'L'};
-    char replacements[5] = {'4', '3', '0', '7', '1'};
-    
-    while (s[i] != '\0') {
-        int j = 0;
-        
-        while (j < 5) {
-            if (s[i] == re[j] || s[i] == re2[j]) {
-                s[i] = replacements[j];
-            }
-            j++;
-        }
-        
-        i++;
-    }
-    return s;
-}
+char *leet(char *s)
+{
+	int i = 0;
+	char re[5] = {'a', 'e', 'o', 't', 'l'};
+	char re2[5] = {'A', 'E', 'O', 'T', 'L'};
+	char replacements[5] = {'4', '3', '0', '7', '1'};
 
-int main() {
-    char input[] = "leet";
-    printf("Original: %s\n", input);
-    
-    char *result = leet(input);
-    printf("Leet Speak: %s\n", result);
+	while (s[i] != '\0')
+	{
+		int j = 0;
 
-    return 0;
-}
+		while (j < 5)
+		{
+			if (s[i] == re[j] || s[i] == re2[j])
+			{
+				s[i] = replacements[j];
+			}
+			j++;
+		}
+		i++;
+	}
