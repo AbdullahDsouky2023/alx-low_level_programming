27e054b8763f48a02b65964dd97675d29c1c9978
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-24 05:21:03 -0700

    adding new files

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcmp - Concatenates two strings.
+ * @s1: The destination string.
+ * @s2: The source string.
+ *
+ * Return: Pointer to the concatenated string.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int length = strlen(s1);
+	int length2 = strlen(s2);
+	int diff;
+
+	while( i <= length2  )
+	{
+		if(s1[i] == s2[i])
+		{
+			diff = 0 ;
+		}
+		else if (s1[i]!=s2[i])
+		{
+			diff = s1[i]-s2[i];
+			break;
+		}
+		i++;
+	}
+	return diff;
+}
