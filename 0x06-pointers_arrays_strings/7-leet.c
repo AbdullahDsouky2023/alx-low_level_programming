e8927f4656f3bf9854fca43945797972c2c21648
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-24 12:17:35 -0700

    adding new files

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include "main.h"
-char *leet(char* s) {
- int i = 0;
+/**
+ * leet - Convert characters in a string to leet speak.
+ * @s: The input string to be converted.
+ *
+ * This function takes an input string and replaces certain characters
+ * with their leet speak equivalents based on predefined replacements.
+ * It performs a case-insensitive conversion for 'a', 'e', 'o', 't', and 'l'.
+ *
+ * Return: A pointer to the modified input string.
+ */
+char *leet(char *s) {
+    int i = 0;
     char re[5] = {'a', 'e', 'o', 't', 'l'};
     char re2[5] = {'A', 'E', 'O', 'T', 'L'};
     char replacements[5] = {'4', '3', '0', '7', '1'};
@@ -10,14 +20,23 @@ char *leet(char* s) {
         int j = 0;
         
         while (j < 5) {
-            if (s[i] == re[j] || s[i]==re2[j]) {
+            if (s[i] == re[j] || s[i] == re2[j]) {
                 s[i] = replacements[j];
-          
             }
             j++;
         }
         
-       i++;
+        i++;
     }
     return s;
+}
+
+int main() {
+    char input[] = "leet";
+    printf("Original: %s\n", input);
+    
+    char *result = leet(input);
+    printf("Leet Speak: %s\n", result);
+
+    return 0;
 }
