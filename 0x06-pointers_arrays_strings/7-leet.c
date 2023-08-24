8c241d056aa4431f4403af8eb14836107693a626
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-24 12:11:09 -0700

    adding new files

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-char* leet(char* s) {
+#include "main.h"
+char *leet(char* s) {
     int i = 0;
     char re[5] = {'a', 'e', 'o', 't', 'l'};
     char replacements[5] = {'4', '3', '0', '7', '1'};
     
     while (s[i] != '\0') {
         int j = 0;
-        int found = 0; // Flag to indicate if a replacement was made
+        int found = 0; 
         
         while (j < 5) {
             if (s[i] == re[j]) {
                 s[i] = replacements[j];
                 found = 1;
-                break; // No need to check the rest of the characters
+                break; 
             }
             j++;
         }
         
         if (!found) {
-            // If no replacement was made, move to the next character
             i++;
         }
     }
