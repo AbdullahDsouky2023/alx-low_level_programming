1b8049f32e35ca0e2ad4e1ddb19e0491fb0810b4
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-24 02:48:40 -0700

    adding new files

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,28 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i=0;
-
-	while(i < n)
-	{
-		dest[i]=src[i];
-		i++;
-	}
-	return dest;
+int i =0;
+    while (i < n) {
+        if (src[i] == '\0' ) {
+            dest[i] = '\0';
+            break;
+        } else {
+            dest[i] = src[i];
+        }
+        i++;
+    }
+     while (i <= n) {
+        dest[i] = 0;
+        i++;
+        if(i == n)
+        {
+            break;
+        }
+    }
+     while (i < 98) {
+        dest[i] = '*';
+        i++;
+    }
+    
+    return dest;
 }
