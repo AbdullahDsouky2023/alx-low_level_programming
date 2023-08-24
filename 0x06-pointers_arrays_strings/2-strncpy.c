a30db6720061267398866753390cda371b1e33f1
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-24 04:24:23 -0700

    adding new files

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,43 +1,22 @@
-#include"main.h"
-
+#include "main.h"
 /**
- * _strcat - Concatenates two strings.
- * @dest: The destination string.
- * @src: The source string.
- *
- * Return: Pointer to the concatenated string.
+ * _strncpy - copies a string
+ * @dest: destination to copy
+ * @src: source to copy
+ * @n: amount of places to copy
+ * Return: value of dest
  */
 char *_strncpy(char *dest, char *src, int n)
-{int i =0;
-    while (i < n) {
-        if (src[i] == '\0' ) {
-            dest[i] = '\0';
-            break;
-        } else {
-            dest[i] = src[i];
-        }
-        i++;
-    }
-    while (i < n) {
-        dest[i] = '\0';
-        i++;
-    }
-     while (i <= 98) {
-       
-             dest[i] = '*';
-         if(i == 97)
-        {
-           dest[97] = '\0';
-           
-        }
-         if(i == 98)
-        {
-           break;
-           
-        }
-        
-        i++;
-    }
-    
-    return dest;
-    }
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
