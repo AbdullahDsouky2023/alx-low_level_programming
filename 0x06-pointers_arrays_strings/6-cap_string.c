419d7c99be95efed27212f0a7f1cfc9dac1901a8
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-24 10:20:23 -0700

    adding new files

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,29 +8,28 @@
  *
  * This function capitalizes the first letter of each word in the string
  * after certain specified characters (space, tab, newline, comma, semicolon,
- * period, exclamation mark, question mark, double quote, parentheses, curly braces).
+ * period, exclamation mark, question mark, double quote, parentheses, curl
+ * y braces).
  *
  * Return: A pointer to the modified string.
  */
 char *cap_string(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' ||
-            s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' ||
-            s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' ||
-            s[i] == '}')
-        {
-            if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-            {
-                s[i + 1] -= 32;
-            }
-        }
-        
-        i++;
-    }
-
-    return s;
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' ||
+				s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' ||
+				s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' ||
+				s[i] == '}')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] -= 32;
+			}
+		}
+		i++;
+	}
+	return (s);
 }
