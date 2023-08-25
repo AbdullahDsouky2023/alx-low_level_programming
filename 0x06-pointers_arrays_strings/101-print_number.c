1b1030657252f65144fba0e4ce31aab036a100ea
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-25 05:54:35 -0700

    adding new files

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
-
+/**
+ * print_number - Concatenates two strings.
+ * @n: The destination string.
+ *
+ * Return: Pointer to the concatenated string.
+ */
 void print_number(int n)
 {
 	if (n < 0)
