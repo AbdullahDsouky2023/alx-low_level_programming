543ec64eedfa2af89812808f134012bddfdb6cce
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-25 05:46:27 -0700

    adding new files

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	putchar((n % 10) + '0');
+}
