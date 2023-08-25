68e69607b6058246b496e65b7a2b077c02bafe95
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-25 02:19:41 -0700

    adding new files

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,24 +1,33 @@
 #include <string.h>
 #include "main.h"
 #include <stdio.h>
-char *rot13(char *s){
-    char *replacement1="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char *replacement2="NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"; 
-    int i = 0;
-    int length=strlen(s);
-    while (i <length)
-    {
-        int j = 0;
-	int reLength= strlen(replacement1);
-        while (j < reLength)
-        {
-        if (replacement1[j] == s[i]){
-            
-        s[i]=replacement2[j];
-        }
-        j++;
-        }
-        i++;
-    }
-    return (s);
+/**
+ * rot13  - Concatenates two strings.
+ * @s: The destination string.
+ *
+ * Return: Pointer to the concatenated string.
+ */
+char *rot13(char *s)
+{
+	char *replacement1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *replacement2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int length = strlen(s);
+
+	while (i < length)
+	{
+		int j = 0;
+		int reLength = strlen(replacement1);
+
+		while (j < reLength)
+		{
+			if (replacement1[j] == s[i])
+			{
+				s[i] = replacement2[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
 }
