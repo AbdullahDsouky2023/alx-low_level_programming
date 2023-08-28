362d6fa44c6648a19f99338e47f8bade9a5e88fd
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-28 05:05:02 -0700

    adding new files

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,26 +1,23 @@
 #include "main.h"
 #include <stdio.h>
- char *_strchr(char *s, char c)
- {
-     char temp[]="fdf";
-     int i =0;
-     int start=0;
-     int j=0;
-     while (s[i] != '\0')
-     {
-         if(s[i] == c){
-         start =i;
-         break;
-         }
-         i++;
-     }
-     while (s[start] != '\0')
-     {
-         temp[j]=s[start];
-         putchar(temp[j]);
-         start++;
-         j++;
-     }
-     return temp;
-    //  printf("%s",temp);
+/**
+ * _strchr - check the code
+ * @s : the statement
+ * @c :the start
+ * Return: pointer to c
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *start;
+
+	while (s[i] != '\0')
+	{
+		if(s[i] == c){
+			start = &s[i];
+			break;
+		}
+		i++;
+	}
+	return (start);
  }
