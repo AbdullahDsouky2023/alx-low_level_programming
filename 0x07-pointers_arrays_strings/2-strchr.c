995473e0961470091f65e756ae7836f3a05288a6
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-28 07:19:15 -0700

    adding new files

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,11 +10,11 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (*s != '\0')
+	while (s[i] != '\0')
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
-			return (s);
+			return (&s[i]);
 		}
 		i++;
 	}
