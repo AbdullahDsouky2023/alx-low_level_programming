660135554d765666c116a5b72adc3980a59c4cc0
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-30 14:41:38 -0700

    adding new files

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,16 +4,17 @@
  *
  * @x: the numbre to raise
  * @y: the power to raise to
- * Return: power 
- */
- 
+ * Return: power
+*/
 int _pow_recursion(int x, int y)
 {
-    if (y ==0 )
-    {
-        return (1);
-    }else if(y<0 ) {
-        return -1;
-    }
-        return (x *_pow_recursion(x,y-1));
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
 }
