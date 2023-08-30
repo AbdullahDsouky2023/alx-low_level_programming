1306d22089ee61b78c7abb43acb045c9f0fd89e0
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-30 10:14:36 -0700

    adding new files

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,16 +7,17 @@
  * Return: nothing
  */
  
- void _puts_recursion(char *s){
-    static int i =0;
-     if (s[i] == '\0')
-     {
-         putchar('\a');
-     }
-     else 
-     {
-        putchar(s[i++]);
-         _puts_recursion(s);
-         
-     }
- }
+ void _puts_recursion(char *s)
+{
+	static int i = 0;
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[i++]);
+		_puts_recursion(s);
+	}
+}
