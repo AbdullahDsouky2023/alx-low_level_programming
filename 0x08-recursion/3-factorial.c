55c356ad1fe608c723a3c12e644f8451083b9f10
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-30 14:24:25 -0700

    adding new files

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,21 +2,22 @@
 /**
  * factorial- function that print statment
  *
- * @s: the statement to be printed
+ * @n: the num to get the factorial to
  * Return: factorial
  */
 
 int factorial(int n)
 {
-    if(n < 0)
-    {
-        return -1;
-    }
-    if(n == 1){
-        return (1);
-    }
-    else {
-    return (n *factorial(n-1));
-
-    }
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
