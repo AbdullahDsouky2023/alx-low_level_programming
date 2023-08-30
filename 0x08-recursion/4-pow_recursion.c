53b9e8ebb181835638b240e4ff8be5e347aca658
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-30 14:36:47 -0700

    adding new files

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  _pow_recursion- function that print statment
+ *
+ * @x: the numbre to raise
+ * @y: the power to raise to
+ * Return: power 
+ */
+ 
+int _pow_recursion(int x, int y)
+{
+    if (y ==0 )
+    {
+        return (1);
+    }else if(y<0 ) {
+        return -1;
+    }
+        return (x *_pow_recursion(x,y-1));
+}
