93f1e5558513afbb272329ef2a038f52856d10c4
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-31 12:03:53 -0700

    adding new files

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,23 +5,23 @@
  * @i: num
  * Return: square root of a number
  */
-int _sqrt_helper(int n,int i)
+int _sqrt_helper(int n, int i)
 {
 	if ((i * i) <= n && ((i + 1) * (i + 1)) > n)
 	{
 		if (i * i == n)
 		{
-			return i;
+			return (i);
 		}
-		return -1;
+		return (-1);
 	}
 	else
 	{
-		return _sqrt_helper(n,i + 1);
-        }
+		return (_sqrt_helper(n, i + 1));
+	}
 	if (n == 1)
 	{
-		return 1;
+		return (1);
 	}
 }
 /**
@@ -31,5 +31,5 @@ int _sqrt_helper(int n,int i)
  */
 int _sqrt_recursion(int n)
 {
-	return _sqrt_helper(n,1);
+	return (_sqrt_helper(n, 1));
 }
