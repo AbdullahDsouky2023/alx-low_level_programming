191b18addc979ea297a66c98ca1efb9389a3c713
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-31 12:00:49 -0700

    adding new files

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,35 @@
 #include "main.h"
 /**
+ *  _sqrt_helper - function that returns the natural square root of a number
+ * @n: num
+ * @i: num
+ * Return: square root of a number
+ */
+int _sqrt_helper(int n,int i)
+{
+	if ((i * i) <= n && ((i + 1) * (i + 1)) > n)
+	{
+		if (i * i == n)
+		{
+			return i;
+		}
+		return -1;
+	}
+	else
+	{
+		return _sqrt_helper(n,i + 1);
+        }
+	if (n == 1)
+	{
+		return 1;
+	}
+}
+/**
  * _sqrt_recursion - function that returns the natural square root of a number
  * @n: num
  * Return: square root of a number
  */
 int _sqrt_recursion(int n)
 {
- int _sqrt_helper(int i)
-      {
-        if ((i * i) <= n && ((i + 1) * (i + 1)) > n) {
-           if (i * i == n) {
-                return i;
-            }
-            return -1;
-        } else {
-            return _sqrt_helper(i + 1);
-        }
-    }
-
-    if (n == 1) {
-        return 1;
-    }
-
-    return _sqrt_helper(1);
-
+	return _sqrt_helper(n,1);
 }
