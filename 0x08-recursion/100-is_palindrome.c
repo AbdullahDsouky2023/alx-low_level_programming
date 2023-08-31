6789ee1d0d96f4b793df15fd52cb4ab9826063ff
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-08-31 15:33:00 -0700

    adding new files

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,38 +3,48 @@
 #include <stdlib.h>
 /**
  *  helper - function that returns the natural square root of a number
- * @n: num
+ * @s: num
  * @i: num
+ * @re:string re
  * Return: square root of a number
  */
 
-int helper(char *s, int i, char *re) {
-	int length =strlen(s);
-    if (i != length) {
-        int reversedChar = strlen(s) - i - 1;
-        if (reversedChar >= 0) {
-            re[i] = s[reversedChar];
-        }
-        i++;
-        helper(s, i, re);
-    } else {
-        re[i] = '\0';
-    }
+int helper(char *s, int i, char *re)
+{
+	int length = strlen(s);
 
-    return 0;
+	if (i != length)
+	{
+		int reversedChar = strlen(s) - i - 1;
+
+		if (reversedChar >= 0)
+		{
+			re[i] = s[reversedChar];
+		}
+		i++;
+		helper(s, i, re);
+	}
+	else
+	{
+		re[i] = '\0';
+	}
+	return (0);
 }
 /**
  *  is_palindrome - function that returns the natural square root of a number
  * @s: num
  * Return: square root of a number
  */
-int is_palindrome(char *s) {
-    char *reversed = (char *)malloc(strlen(s) + 1);
-    helper(s, 0, reversed);
-    if (strcmp(reversed, s) == 0) {
-        free(reversed);
-        return 1;
-    }
-    free(reversed);
-    return 0;
+int is_palindrome(char *s)
+{
+	char *reversed = (char *)malloc(strlen(s) + 1);
+
+	helper(s, 0, reversed);
+	if (strcmp(reversed, s) == 0)
+	{
+		free(reversed);
+		return (1);
+	}
+	free(reversed);
+	return (0);
 }
