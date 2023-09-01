e6ce3a77575c322324a73c2f396f1c544a2c8701
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-01 14:40:15 -0700

    alx software enginner

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,72 +1,54 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-/**
- * calculate_min - check the code
- * @i :count
- * @num : value
- * Return:nothing.
- */
-void calculate_min(int i, int num)
-{
-	int values[] = {1, 2, 5, 10, 25};
+#include <stdlib.h>
 
-	while (i < 5)
-	{
-		if (values[i] >= num)
-		{
-			if (values[i] == num)
-			{
-				printf("%d\n", 1);
-				break;
-			}
-			else
-			{
-				printf("%d\n", num - values[i - 1]);
-				break;
-			}
-		}
-		else if (num > 25)
-		{
-			if (num % 25 == 0)
-			{
-				printf("%d\n", num / 25);
-				break;
-			}
-			else
-			{
-				printf("%d\n", num / 25 + num % 25);
-				break;
-			}
-		}
-		i++;
-	}
-}
 /**
- * main - check the code
- * @argc :count
- * @argv : value
- * Return: Always 0.
+ * main - Prints the minimum number of coins to
+ *        make change for an amount of money.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If the number of arguments is not exactly one - 1.
+ *         Otherwise - 0.
  */
 int main(int argc, char *argv[])
 {
-	int i = 0;
-	int num;
+	int cents, coins = 0;
 
-	if (argc !=  2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	num = atoi(argv[1]);
-	if (num < 0)
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
 	{
-		printf("%d\n", 0);
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
 	}
-	else
-	{
-	calculate_min(i, num);
-	}
+
+	printf("%d\n", coins);
+
 	return (0);
 }
