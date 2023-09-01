9aaf5b71950ddbe2265c624ab3b6bbd7de2aca67
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-01 07:35:36 -0700

    alx software enginner

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - check the code
+ * @argc :count
+ * @argv : value
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+    if (argc ==  1)
+    {
+        printf("Error");
+    }
+    else
+    {
+        printf("%d", atoi(argv[1]) + atoi(argv[2]));
+    }
+        return (0);
+}
