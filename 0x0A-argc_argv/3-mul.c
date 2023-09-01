13e34ae53b09001f94b4f5fb2416eb21e7e9aadb
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-01 07:36:31 -0700

    alx software enginner

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,11 @@ int main(int argc, char *argv[])
 {
     if (argc ==  1)
     {
-        printf("Error");
+        printf("Error\n");
     }
     else
     {
-        printf("%d", atoi(argv[1]) + atoi(argv[2]));
+        printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
     }
         return (0);
 }
