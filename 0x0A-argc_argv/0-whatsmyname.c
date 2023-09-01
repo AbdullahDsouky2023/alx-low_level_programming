1e8e284541a11ac078c915c912d2b30d08c39c28
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-01 07:10:15 -0700

    alx software enginner

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main(int argc,char *argv[])
+/*
+ * main - entry point
+ * Return: 0
+ * */
+int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	(void)argc;
 	return (0);
 }
