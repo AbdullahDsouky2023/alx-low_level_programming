60085f512a52e1b52d0a82061888ca7dea216c9d
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-01 13:32:37 -0700

    alx software enginner

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,6 +3,47 @@
 #include <string.h>
 #include <ctype.h>
 /**
+ * calculate_min - check the code
+ * @i :count
+ * @num : value
+ * Return:nothing.
+ */
+void calculate_min(int i, int num)
+{
+	int values[] = {1, 2, 5, 10, 25};
+
+	while (i < 5)
+	{
+		if (values[i] >= num)
+		{
+			if (values[i] == num)
+			{
+				printf("%d\n", 1);
+				break;
+			}
+			else
+			{
+				printf("%d\n", num - values[i - 1]);
+				break;
+			}
+		}
+		else if (num > 25)
+		{
+			if (num % 25 == 0)
+			{
+				printf("%d\n", num / 25);
+				break;
+			}
+			else
+			{
+				printf("%d\n", num / 25 + num % 25);
+				break;
+			}
+		}
+		i++;
+	}
+}
+/**
  * main - check the code
  * @argc :count
  * @argv : value
@@ -10,51 +51,20 @@
  */
 int main(int argc, char *argv[])
 {
-    
-    int values[]={1,2,5,10,25};
-    int i =0;
-    int num;
-    if(argc < 2 )
-    {
-                        printf("Error\n");
-                        return (1); 
+	int i = 0;
+	int num;
 
-    }
-   
-    num = atoi(argv[1]);
-    if(num < 0){
-	    printf("%d\n",0);
-        return (0);
-    }
-    while (i < 5)
-    {
-        if (values[i] >= num)
-        {
-            if (values[i] == num)
-            {
-                printf("%d\n", 1);
-		break;
-            }
-            else
-            {
-              printf("%d\n", num- values[i-1]);
-	      break;
-            }
-        }
-        else if (num > 25)
-        {
-            if (num % 25 == 0)
-            {
-                printf("%d\n", num / 25);
-		break;
-            }
-            else
-            {
-                printf("%d\n", num / 25 + num % 25);
-		break;
-            }
-        }
-        i++;
-    }
-        return (0);
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num = atoi(argv[1]);
+	if (num < 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	calculate_min(i, num);
+	return (0);
 }
