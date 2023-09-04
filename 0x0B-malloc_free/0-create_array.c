392814d61543ad0320398b84aed50f65f52fe768
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-04 03:55:06 -0700

    alx software enginner

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
  */
 char *create_array(unsigned int size, char c){
     char *arr = (char *) malloc(sizeof(char) * size);
-    int i;
+    unsigned  int i;
     if(size == 0 || arr == NULL)
     {
         return NULL;
