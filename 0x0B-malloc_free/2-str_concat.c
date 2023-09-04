2dbc569856728ada56ca13568e9d7457db7974a9
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-04 06:02:48 -0700

    alx software enginner

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,8 @@ char *str_concat(char *s1, char *s2)
 {
 	int i = 0;
 	int j = 0;
+	int len1 = strlen(s1);
+       int len2 = strlen(s2);	
     char *concatString =(char *) malloc(sizeof(char) *
      (strlen(s1) + strlen(s2)) +1 );
      
@@ -23,12 +25,12 @@ char *str_concat(char *s1, char *s2)
      }
      int i =0;
      int j = 0;
-     while (i < strlen(s1))
+     while (i < len1)
      {
          concatString[i] = s1[i];
          i++;
      }
-     while (j < strlen(s2))
+     while (j < len2)
      {
          concatString[i] = s2[j];
          i++;
