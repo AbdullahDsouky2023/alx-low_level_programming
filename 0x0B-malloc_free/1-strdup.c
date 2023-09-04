c13289bc07ecdf270b2241f5da12d2782bd4ce84
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-04 05:24:26 -0700

    alx software enginner

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup- copy a string and return pointer
+ * @str: tteh str to copy
+ *
+ * Return: pointer to str .
+ */
+char *_strdup(char *str)
+{
+	int length = 0;
+	int i = 0;
+	char *s = (char *) malloc(sizeof(char) * length);
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	while (i < length)
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
