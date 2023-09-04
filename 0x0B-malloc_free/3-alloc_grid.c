e3a777621b196ac46c13a4d0899d52658eff090e
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-04 13:12:18 -0700

    alx software enginner

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,72 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-
-int **grid_to_free = NULL;
-/**
- * free_array - prints a grid of integers
- * @width: width of the grid
- * @height: height of the grid
- *
- * Return: Nothing.
- */
-void free_array(int **grid)
-{
-if (grid_to_free != NULL)
-    {
-        int i = 0;
-        while (grid[i] != NULL)
-        {
-            free(grid[i]);
-            i++;
-        }
-        free(grid);
-    }
-}
+#include <stdlib.h>
 
 /**
- * free_array_wrapper - prints a grid of integers
- * Return: Nothing.
- */
-void free_array_wrapper(void)
-{
-    if (grid_to_free != NULL)
-    {
-        free_array(grid_to_free);
-    }
-}
-/**
- * alloc_grid - prints a grid of integers
- * @width: width of the grid
- * @height: height of the grid
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the array.
+ * @height: height of the array.
  *
- * Return: Nothing.
+ * Return: pointer of an array of integers
  */
 int **alloc_grid(int width, int height)
 {
-	int **arr = (int **)malloc(height * sizeof(int *));
-	int i = 0;
+	int **gridout;
+	int i, j;
 
-	if (width <= 0 || height <= 0)
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	gridout = malloc(height * sizeof(int *));
+	if (gridout == NULL)
 	{
+		free(gridout);
 		return (NULL);
 	}
-	if (arr == NULL)
+
+	for (i = 0; i < height; i++)
 	{
-		fprintf(stderr, "Memory allocation failed for rows.\n");
-		exit(1);
-	}
-	while (i < height)
-	{
-		arr[i] = (int *)malloc(width * sizeof(int));
-		if (arr[i] == NULL)
+		gridout[i] = malloc(width * sizeof(int));
+		if (gridout[i] == NULL)
 		{
-			fprintf(stderr, "Memory allocation failed for columns.\n");
-			exit(1);
+			for (i--; i >= 0; i--)
+				free(gridout[i]);
+			free(gridout);
+			return (NULL);
 		}
-		i++;
 	}
-	grid_to_free = arr;
-	atexit(free_array_wrapper);
-	return (arr);
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			gridout[i][j] = 0;
+
+	return (gridout);
 }
