5a1cd39516ad0d68528288ff507899663e44f5f8
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-05 13:10:55 -0700

    alx software enginner

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char **strtow(char *str)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int count = 0;
+    int words = 0;
+    char **arr;
+
+    while (i < strlen(str))
+    {
+        if (str[i] != ' ')
+        {
+            if (str[i + 1] == ' ')
+            {
+                words++;
+            }
+        }
+        i++;
+    }
+
+    // Allocate memory for an array of char*
+    arr = (char **)malloc(sizeof(char *) * (words + 1));
+
+    if (arr == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed for arr.\n");
+        exit(1);
+    }
+
+    // Reset counters for a new pass
+    i = 0;
+    words = 0;
+
+    while (k < strlen(str))
+    {
+        if (str[k] != ' ')
+        {
+            if (count == 0)
+            {
+                // Allocate memory for each word
+                arr[words] = (char *)malloc(sizeof(char) * 50);
+                if (arr[words] == NULL)
+                {
+                    fprintf(stderr, "Memory allocation failed for arr[%d].\n", words);
+                    exit(1);
+                }
+            }
+
+            arr[words][count] = str[k];
+            count++;
+
+            if (str[k + 1] == ' ' || str[k + 1] == '\0')
+            {
+                arr[words][count] = '\0'; // Null-terminate the word
+                words++;
+                count = 0;
+            }
+        }
+
+        k++;
+    }
+
+    // Null-terminate the array of strings
+    arr[words] = NULL;
+
+    return arr;
+}
