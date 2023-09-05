be2bbd0a993ea52bb39e6f788c88447d24785261
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-05 14:30:28 -0700

    alx software enginner

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,101 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int get_words_length(char *str)
-{
-    int i =0;
-    int words = 1;
-    int length =strlen(str);
-     while (i < length)
-    {
-        if (str[i] != ' ')
-        {
-            if (str[i + 1] == ' ')
-            {
-                words++;
+
+int get_words_length(char *str) {
+    int i = 0;
+    int words = 0;
+    int length = strlen(str);
+
+    for (i = 0; i < length; i++) {
+        if (str[i] != ' ') {
+            words++;
+            while (str[i] != ' ' && str[i] != '\0') {
+                i++;
             }
+        } else {
+            i++;
         }
-        i++;
     }
+
     return words;
 }
-int *get_word_length(char *str)
-{
-    int words = 0;
-    int k =0;
-    int *counts=(int *)malloc (sizeof(int) * words);
-    int length=strlen(str);
-    while (k < length)
-    {
-        if (str[k] != ' ')
-        {
-            counts[words]+=1;
-            if (str[k + 1] == ' ' || str[k + 1] == '\0')
-            {
-                words++;
-            }
-        }
-        k++;
+
+int *get_word_length(char *str, int wordsCount) {
+    int *counts = (int *)malloc(sizeof(int) * wordsCount);
+    if (counts == NULL) {
+        fprintf(stderr, "Memory allocation failed for counts.\n");
+        exit(1);
     }
+
+    int i = 0;
+    int length = strlen(str);
+    int wordIndex = 0;
+
+    while (i < length) {
+        if (str[i] != ' ') {
+            counts[wordIndex] = 0;
+
+            while (str[i] != ' ' && str[i] != '\0') {
+                counts[wordIndex]++;
+                i++;
+            }
+
+            wordIndex++;
+        } else {
+            i++;
+        }
+    }
+
     return counts;
 }
-char **seperate_sting(char *str,int wordsCount,int *counts)
-{
-    int k =0;
-    int words = 0;
-    int count = 0;
-    int length=strlen(str);
-    
-    char **arr = (char **)malloc(sizeof(char *) * (wordsCount + 1));
 
-    while (k < length)
-    {
-        if (str[k] != ' ')
-        {
-            if (count == 0)
-            {
-                arr[words] = (char *)malloc(sizeof(char) * counts[words]);
-                if (arr[words] == NULL)
-                {
-                    fprintf(stderr, "Memory allocation failed for arr[%d].\n", words);
+char **separate_string(char *str, int wordsCount, int *counts) {
+    int i = 0;
+    int wordIndex = 0;
+    int count = 0;
+    int length = strlen(str);
+
+    char **arr = (char **)malloc(sizeof(char *) * (wordsCount + 1));
+    if (arr == NULL) {
+        fprintf(stderr, "Memory allocation failed for arr.\n");
+        exit(1);
+    }
+
+    for (i = 0; i < length; i++) {
+        if (str[i] != ' ') {
+            if (count == 0) {
+                arr[wordIndex] = (char *)malloc(sizeof(char) * (counts[wordIndex] + 1));
+                if (arr[wordIndex] == NULL) {
+                    fprintf(stderr, "Memory allocation failed for arr[%d].\n", wordIndex);
                     exit(1);
                 }
             }
 
-            arr[words][count] = str[k];
+            arr[wordIndex][count] = str[i];
             count++;
 
-            if (str[k + 1] == ' ' || str[k + 1] == '\0')
-            {
-                arr[words][count] = '\0'; 
-                words++;
+            if (str[i + 1] == ' ' || str[i + 1] == '\0') {
+                arr[wordIndex][count] = '\0';
+                wordIndex++;
                 count = 0;
             }
         }
-
-        k++;
     }
+
     return arr;
 }
-char **strtow(char *str)
-{
-    int words = 0;
-    int length =strlen(str);
-    char **arr;
-    int *counts;
-    if (length == 1 || length == 0)
-    {
+
+char **strtow(char *str) {
+    int wordsCount = get_words_length(str);
+
+    if (wordsCount == 0) {
         return NULL;
     }
-    words=get_words_length(str);
-    counts = get_word_length(str);
-    arr=seperate_sting(str,words,counts);
-    if (arr == NULL)
-    {
+
+    int *counts = get_word_length(str, wordsCount);
+    char **arr = separate_string(str, wordsCount, counts);
+
+    if (arr == NULL) {
         fprintf(stderr, "Memory allocation failed for arr.\n");
         exit(1);
     }
-    arr[words] = NULL;
+
+    arr[wordsCount] = NULL;
 
     return arr;
 }
