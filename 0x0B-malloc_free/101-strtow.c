479a72c6a891204c7649cff3359b0077fc4dbeef
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-05 13:43:04 -0700

    alx software enginner

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -43,7 +43,7 @@ char **strtow(char *str)
         {
             if (count == 0)
             {
-                arr[words] = (char *)malloc(sizeof(char) * 5);
+                arr[words] = (char *)malloc(sizeof(char) * 6);
                 if (arr[words] == NULL)
                 {
                     fprintf(stderr, "Memory allocation failed for arr[%d].\n", words);
