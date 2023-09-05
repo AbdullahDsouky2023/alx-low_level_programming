050c297141b00fc0778d94522c89f75b5f6eaf6f
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-05 10:43:11 -0700

    alx software enginner

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,29 +1,32 @@
-// #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include "main.h"
 /**
  * argstostr - check the code for ALX School students.
  *@ac:ff
  * @av:fss
  * Return: Always 0.
  */
+
 char *argstostr(int ac, char **av)
 {
+
+    int total_length = 0;
+    int i;
+    char *arr ;
     if (ac == 0 || av == NULL)
     {
         return NULL;
     }
 
-    int total_length = 0;
-    int i;
-
     for (i = 0; i < ac; i++)
     {
-        total_length += strlen(av[i]) + 1;
+        total_length += strlen(av[i]) + 1; 
     }
 
-    char *arr = (char *)malloc(total_length * sizeof(char));
+    *arr =  (char *)malloc(total_length * sizeof(char)); 
+
     if (arr == NULL)
     {
         return NULL;
@@ -33,8 +36,8 @@ char *argstostr(int ac, char **av)
 
     for (i = 1; i < ac; i++)
     {
-        strcat(arr, "\n");
-        strcat(arr, av[i]);
+        strcat(arr, "\n"); 
+        strcat(arr, av[i]); 
     }
 
     return arr;
