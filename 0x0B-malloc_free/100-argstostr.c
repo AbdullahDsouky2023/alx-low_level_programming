4a20a5425af0b18a1abaaffac00ad9876f66b82b
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-05 11:08:09 -0700

    alx software enginner

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,35 +11,29 @@
 
 char *argstostr(int ac, char **av)
 {
+	int total_length = 0;
+	int i;
+	char *arr;
 
-    int total_length = 0;
-    int i;
-    char *arr ;
-    if (ac == 0 || av == NULL)
-    {
-        return NULL;
-    }
-
-    for (i = 0; i < ac; i++)
-    {
-        total_length += strlen(av[i]) +1; 
-    }
-
-    arr = (char*) malloc(total_length * sizeof(char)+ 1); 
-
-    if (arr == NULL)
-    {
-        return NULL;
-    }
-
-    strcpy(arr, av[0]);
-
-    for (i = 1; i < ac; i++)
-    {
-        strcat(arr, "\n"); 
-        strcat(arr, av[i]); 
-    }
-
-        strcat(arr, "\n"); 
-    return arr;
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		total_length += strlen(av[i]) + 1;
+	}
+	arr = (char *) malloc(total_length * sizeof(char) + 1);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(arr, av[0]);
+	for (i = 1; i < ac; i++)
+	{
+		strcat(arr, "\n");
+		strcat(arr, av[i]);
+	}
+	strcat(arr, "\n");
+	return (arr);
 }
