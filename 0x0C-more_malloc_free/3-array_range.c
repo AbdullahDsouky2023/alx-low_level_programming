ef7f7fe913c15fdad4dac0e2d1ee65244370d8d9
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-07 06:53:46 -0700

    alx software enginner

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,24 +4,26 @@
 #include <string.h>
 
 /**
- * array_rangeloc - array_range
+ * array_range - array_range
  * @min: the address of memory to print
  * @max: the size of the memory to print
  *
  * Return: array of int.
  */
-int *array_range(int min, int max){
-    int *ptr;
-    int i = min;
-    ptr = (int *) malloc(sizeof(int) * max + 1);
-    if(ptr == NULL || min > max)
-    {
-        return (NULL);
-    }
-    while(i <= max)
-    {
-        ptr[i]=i;
-        i++;
-    }
-    return (ptr);
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i = min;
+
+	ptr = (int *) malloc(sizeof(int) * max + 1);
+	if(ptr == NULL || min > max)
+	{
+		return (NULL);
+	}
+	while(i <= max)
+	{
+		ptr[i] = i;
+		i++;
+	}
+	return (ptr);
 }
