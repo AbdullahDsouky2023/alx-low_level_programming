80ed77f5e8a13db6fb7085d22c4bc968f760e601
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-07 12:28:31 -0700

    alx software enginner

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,8 +14,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *conStr;
 	unsigned int i = 0;
 	unsigned int j = 0;
-	unsigned int length1;
-	unsigned int length2;
+	unsigned int length1 = 0;
+	unsigned int length2 = 0;
 
 	if (s1 == NULL)
 	{
