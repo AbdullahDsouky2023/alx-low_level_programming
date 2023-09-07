774402065f6aae90005535b623fe9e1e0abc2045
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-07 05:53:39 -0700

    alx software enginner

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,43 +11,43 @@
  */
  
 char *string_nconcat(char *s1, char *s2, unsigned int n)
- {
-     char* conStr;
-     unsigned int i = 0;
-     unsigned int j =0;
-     unsigned int length1;
-     unsigned int length2;
-     
-     if (s1 == NULL)
-     {
-         s1="";
-     }
-     else if (s2 == NULL)
-     {
-         s2="";
-     }
-     conStr= (char *) malloc(strlen(s1) + strlen(s2) + 1);
-     if (conStr == NULL)
-     {
-         return (NULL);
-     }
-      length1 = strlen(s1);
-      length2 = strlen(s2);
-     if (n >= length2)
-     {
-         n = length2;
-     }
-     while (i < length1)
-     {
-         conStr[i]=s1[i];
-         i++;
-     }
-     while (j < n)
-     {
-         conStr[i]=s2[j];
-         i++;
-         j++;
-     }
-     conStr[i] = '\0';
-     return (conStr);
+{
+	char* conStr;
+	unsigned int i = 0;
+	unsigned int j =0;
+	unsigned int length1;
+	unsigned int length2;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	else if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	conStr= (char *) malloc(strlen(s1) + strlen(s2) + 1);
+	if (conStr == NULL)
+	{
+		return (NULL);
+	}
+	length1 = strlen(s1);
+	length2 = strlen(s2);
+	if (n >= length2)
+	{
+		n = length2;
+	}
+	while (i < length1)
+	{
+		conStr[i]=s1[i];
+		i++;
+	}
+	while (j < n)
+	{
+		conStr[i]=s2[j];
+		i++;
+		j++;
+	}
+	conStr[i] = '\0';
+	return (conStr);
  }
