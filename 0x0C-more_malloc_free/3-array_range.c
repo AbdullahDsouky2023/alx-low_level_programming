ec7ba18b9b6a9d4489ec029d052338c99ed3e19c
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-07 07:23:28 -0700

    alx software enginner

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,10 @@ int *array_range(int min, int max)
 	int i = 0;
 	int num;
 
-	if (min > max) return (NULL);
+	if (min > max)
+	{
+		return (NULL);
+	}
 	ptr = (int *) malloc(sizeof(int) * (max - min + 1));
 	if (ptr == NULL)
 	{
