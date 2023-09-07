05cfa690b84880dd0aaab41ebf17a8067fb3bc11
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-07 04:27:14 -0700

    alx software enginner

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,13 +7,12 @@
  * @b : the interger to reserve to
  * Return: Always 0.
  */
- 
 void *malloc_checked(unsigned int b)
 {
-    int* ptr = malloc(sizeof(b));
+    void *ptr = malloc(sizeof(b));
     if(ptr == NULL)
     {
        exit(98);
     }
    return (ptr);
-}
+} 
