6d5edd8adcba7cce70ad84ec0941750561bc8a26
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-08 09:35:25 -0700

    alx software enginner

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _realloc - check the code for
+ * @ptr :pointer
+ * @old_size : old 
+ * @new_size : new size
+ * Return: Always 0.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+    void *ptr2 = malloc(new_size);
+    if (ptr2 == NULL) {
+        return NULL; 
+    }
+
+    
+    if (ptr != NULL) {
+        memcpy(ptr2, ptr, old_size);
+        free(ptr);
+    }
+
+    return ptr2;
+}
