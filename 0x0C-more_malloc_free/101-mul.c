668e18ae9377a7b2195656c74d9c881f3eba7bc9
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-08 12:56:34 -0700

    alx software enginner

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,7 +14,7 @@
 
 int main(int argc,char *argv[])
 {   
-    int num1, num2 , mul;
+    int num1, num2 , mul = 0;
     if (argc != 3)
     {
         printf("Error\n");
@@ -22,7 +22,7 @@ int main(int argc,char *argv[])
     }
      num1 = atoi(argv[1]);
      num2 = atoi(argv[2]);
-    if(isdigit(argv[1]) || isdigit(argv[2]))
+    if(!isdigit(*argv[1]) || !isdigit(*argv[2]))
     {
         printf("Error\n");
         exit(98);
