38b7fc017c69c529c2ef6a13abcb0d38c583f26f
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-08 13:00:45 -0700

    alx software enginner

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-// Function to check if a string consists of digits only
 int isNumeric(const char *str) {
     while (*str) {
         if (!isdigit(*str)) {
@@ -13,33 +12,33 @@ int isNumeric(const char *str) {
     return 1;
 }
 
-// Function to multiply two positive numbers
 int multiply(int num1, int num2) {
     return num1 * num2;
 }
 
 int main(int argc, char *argv[]) {
+    int num1, num2, result;
     if (argc != 3) {
         printf("Error\n");
-        return 98; // Indicate incorrect number of arguments
+        return 98; 
     }
 
     if (!isNumeric(argv[1]) || !isNumeric(argv[2])) {
         printf("Error\n");
-        return 98; // Indicate non-numeric input
+        return 98; 
     }
 
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
+     num1 = atoi(argv[1]);
+     num2 = atoi(argv[2]);
 
     if (num1 < 0 || num2 < 0) {
         printf("Error\n");
-        return 98; // Indicate negative input (assuming positive numbers only)
+        return 98; 
     }
 
-    int result = multiply(num1, num2);
+     result = multiply(num1, num2);
     printf("%d\n", result);
 
-    return 0; // Indicate success
+    return 0; 
 }
 
