ddd12e807b29fc46592e154720a221e648ebd2a5
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-11 14:45:56 -0700

    alx software enginner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,15 +11,15 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDogay = malloc(sizeof(dog_t));
+
 	if (newDogay == NULL)
 	{
 		return (NULL);
 	}
 	newDogay->name = malloc(strlen(name) + 1);
 	newDogay->owner = malloc(strlen(owner) + 1);
-	
 	strcpy(newDogay->name, name);
 	newDogay->age = age;
-	strcpy(newDogay->owner , owner);
+	strcpy(newDogay->owner, owner);
 	return (newDogay);
 }
