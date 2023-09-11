96259d0559bcdb17e4dcda78ed200b244b127b54
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-11 14:01:22 -0700

    alx software enginner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,19 +1,22 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * print_dog  - check the code
- * @d : pointer
- * Description :struct
+ * new_dog  - check the code
+ * @name : pointer
+ * @age : pointer
+ * @owner : pointer
+ * Return:struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  dog_t *newDogay =(dog_t *) malloc(sizeof(dog_t));
-  if (newDogay == NULL)
-  {
-    return (NULL);
-  }
-  newDogay->name = name;
-  newDogay->age = age;
-  newDogay->owner = owner;
-  return (newDogay);
+	dog_t *newDogay = (dog_t *) malloc(sizeof(dog_t));
+
+	if (newDogay == NULL)
+	{
+		return (NULL);
+	}
+	newDogay->name = name;
+	newDogay->age = age;
+	newDogay->owner = owner;
+	return (newDogay);
 }
