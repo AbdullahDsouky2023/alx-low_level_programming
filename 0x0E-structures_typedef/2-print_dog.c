f9c9698559f29d4b71dde984259334595bbba00c
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-11 13:20:09 -0700

    alx software enginner

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		printf(" ");
+		return;
 	}
 	printf("Name: %s\n", (*d).name == NULL ? "(nil)" : (*d).name);
 	printf("Age: %f\n", !(*d).age  ? 0 : (*d).age);
