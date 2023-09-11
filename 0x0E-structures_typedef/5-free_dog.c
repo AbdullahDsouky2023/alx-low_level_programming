f2e1f18da5c597ff3c38d94c8bf6edc67d0a3ea7
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-11 15:01:03 -0700

    alx software enginner

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog  - check the code
+ * @d : pointer
+ * Return:struct
+ */
+void free_dog(dog_t *d)
+{
+  free(d->name);
+  free(d->owner);
+  free(d);
+}
