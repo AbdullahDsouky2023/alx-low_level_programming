b255ffab2bd9212e68bd48fc5a654dea3b847f21
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-13 05:42:39 -0700

    alx software enginner

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * array_iterator - prints an integer
- * @array: the array to do the action on 
+ * @array: the array to do the action on
  * @size:size of the array
- * @action: action to be done on each element 
- * Return: Nothing.
+ * @action: action to be done on each element
+ * * Return: Nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
