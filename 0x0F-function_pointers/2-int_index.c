cc9f36a8aedda9342d4f2d54ca180ef8ec53d7ca
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-13 06:35:12 -0700

    alx software enginner

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 /**
  * int_index - search for an integer
- * @array: the array to check
+ *@array: the array to check
  * @size: the size to check
- * @cmp: the cmp to check
+ * *@cmp: the cmp to check
  *
  * Return: 0 if false, something else otherwise.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-  int i =0;
-  if (array && size && cmp)
-  {
-    if (size <= 0)
-    {
-      return (-1);
-    }
-  while(i < size)
-    {
-      if(cmp(array[i]))
-      {
-        return (i);
-      }
-      i++;
-    }
-  }
-  return (-1);
+	int i = 0;
+
+	if (array && size && cmp)
+	{
+		if (size <= 0)
+		{
+			return (-1);
+		}
+		while (i < size)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+			i++;
+		}
+	}
+	return (-1);
 }
