df85b3c84746ccfcad716218ca4d8803b77acab6
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-13 05:20:13 -0700

    alx software enginner

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,10 @@
 /**
  * print_name - prints a name as is
  * @name: name of the person
- * @(*f)(char *) : pointer to functoin 
+ * @f : pointer to functoin
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
- f(name);
+	f(name);
 }
