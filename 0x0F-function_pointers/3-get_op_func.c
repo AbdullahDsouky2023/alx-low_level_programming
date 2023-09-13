6c4ee73f7fcfb21f98d131cd1a9fb1ed5deb5fa9
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-13 13:25:23 -0700

    alx software enginner

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,28 +2,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+/**
+ * get_op_func - check if a number is equal to 98
+ * @s: the second intger
+ * Return: result
+ */
 int (*get_op_func(char *s))(int a, int b)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op != NULL) {
-    if (strcmp(ops[i].op, s) == 0) {
-      return ops[i].f;
-    }
-    i++;
-  }
-  if (ops[i].op == NULL) {
-    printf("Error");
-    exit(99);
-  }
-  return (NULL); 
-
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -29,7 +29,7 @@ int op_sub(int a, int b)
 */
 int op_mul(int a, int b)
 {
-        return (a * b);
+	return (a * b);
 }
 /**
  * op_div - check if a number is equal to 98
@@ -44,7 +44,7 @@ int op_div(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-        return (a / b);
+	return (a / b);
 }
 /**
  * op_mod - check if a number is equal to 98
@@ -54,11 +54,10 @@ int op_div(int a, int b)
 */
 int op_mod(int a, int b)
 {
-
-        if (b == 0)
-        {
-                printf("Error\n");
-                exit(100);
-        }
-        return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
