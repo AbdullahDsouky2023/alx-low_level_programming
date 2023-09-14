ecce687fe885ccc7cedd3f51f23508d48c18309b
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-14 10:33:57 -0700

    alx software enginner

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -6,30 +6,33 @@
 /**
  * print_strings - check the code
  * @n:count
- * @seperator:count
+ * @separator:count
  * Return: Always 0.
  */
-void print_strings(const char *separator, const unsigned int n, ...){
-    va_list ptr;
-   unsigned  int i = 0;
-   char *state;
-    va_start(ptr, n);
-    while (i < n)
-    {
-        state = va_arg(ptr,char*);
-        if (state == NULL){
-        printf("(nil");
-            
-        }else {
-        printf("%s",state);
-            
-        }
-        if(separator != NULL && i != n - 1)
-        {
-            printf("%s",separator);
-        }
-        i++;
-    }
-    printf("\n");
-    va_end(ptr);
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned  int i = 0;
+	char *state;
+
+	va_start(ptr, n);
+	while (i < n)
+	{
+		state = va_arg(ptr, char*);
+		if (state == NULL)
+		{
+			printf("(nil");
+		}
+		else
+		{
+			printf("%s", state);
+		}
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ptr);
 }
