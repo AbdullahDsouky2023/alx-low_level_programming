ecf35e40808e702b68cdc57c0632442803f79574
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-14 07:43:01 -0700

    alx software enginner

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,22 +4,23 @@
 
 
 /**
- * sum_them_all - check the code
+ * print_numbers - check the code
  * @n:count
- * Return: Always 0.
+ * @separator: spac
+ * Return: nothin
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ptr;
 	unsigned  int i = 0;
-	va_start(ptr,n);
 
+	va_start(ptr, n);
 	while (i < n)
 	{
 		printf("%d", va_arg(ptr, int));
-		if ((i != n-1) && (separator != NULL)  )
+		if ((i != n - 1) && (separator != NULL))
 		{
-			printf("%s",separator);
+			printf("%s", separator);
 		}
 		i++;
 	}
