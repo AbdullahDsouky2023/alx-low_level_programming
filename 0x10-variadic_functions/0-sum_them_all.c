fbc02904338c589fab4df777b7658b810af7aaf4
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-14 06:35:03 -0700

    alx software enginner

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,24 +3,24 @@
 
 
 /**
- * main - check the code
- *
+ * sum_them_all - check the code
+ * @n:count
  * Return: Always 0.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	
-        int sum = 0;
-	unsigned int i =0;
+	int sum = 0;
+	unsigned int i = 0;
 	va_list nptr;
+
 	if (n == 0)
 	{
-		return(0);
+		return (0);
 	}
-	va_start(nptr,n);
+	va_start(nptr, n);
 	while (i < n)
 	{
-		sum+=va_arg(nptr, int);
+		sum += va_arg(nptr, int);
 		i++;
 	}
 	va_end(nptr);
