97b568a94e83dc5c2ba36f250edba5a66fcd3061
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-14 07:49:34 -0700

    alx software enginner

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+
+/**
+ * print_strings - check the code
+ * @n:count
+ * @seperator:count
+ * Return: Always 0.
+ */
+void print_strings(const char *separator, const unsigned int n, ...){
+    va_list ptr;
+    int i = 0;
+    char *sate;
+    va_start(ptr, n);
+    while (i < n)
+    {
+        state=va_arg(ptr,char*)
+        if (state == NULL){
+        printf("(nil");
+            
+        }else {
+        printf("%s",state);
+            
+        }
+        if(separator != NULL && i != n - 1)
+        {
+            printf("%s",separator);
+        }
+        i++;
+    }
+    printf("\n");
+    va_end(ptr);
+}
