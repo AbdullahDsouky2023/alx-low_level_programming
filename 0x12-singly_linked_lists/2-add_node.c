91708b0ae024185cce1d47794ae4d876336deb07
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-24 13:35:27 -0700

    alx software enginner

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node  - singly linked list
+ * Return:0
+ * @head: points to the next node
+ * @str: points to the next node
+ * Description: singly linked list node structure
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	new_node->str = str;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
