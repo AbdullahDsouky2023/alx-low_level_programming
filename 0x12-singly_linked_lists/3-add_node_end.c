9e0e7650bde9ac559b872b1837ca51c0ded8ef15
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-24 15:34:49 -0700

    alx software enginner

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node_end - check the code
+ * @head:the head
+ * @str:the string to copy
+ * Return: Always 0.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *last = *head;
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return;
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
+	return (new_node);
+}
