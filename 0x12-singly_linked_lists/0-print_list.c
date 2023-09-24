5bc26b3f50db7b18ea18833f3423728dbe48a6b6
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-24 12:41:28 -0700

    alx software enginner

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_list  - singly linked list
+ * @h: points to the next node
+ * Return:0
+ * Description: singly linked list node structure
+ */
+
+int print_list(const list_t *h)
+{
+	unsigned int num = 0;
+
+	while (h != NULL)
+	{
+		num++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (num);
+}
