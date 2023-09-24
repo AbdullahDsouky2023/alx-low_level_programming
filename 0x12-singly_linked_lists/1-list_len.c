7c6d938a7d507d4c9cbddf7b65e6ba55900d7490
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-24 13:08:36 -0700

    alx software enginner

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * list_len  - singly linked list
+ * @h: points to the next node
+ * Return:0
+ * Description: singly linked list node structure
+ */
+
+size_t list_len(const list_t *h)
+{
+	int len = 0;
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
