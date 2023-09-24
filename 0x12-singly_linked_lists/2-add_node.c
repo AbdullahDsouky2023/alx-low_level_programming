462ba5910574511852e319d2f6df77bce657614d
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-24 14:24:23 -0700

    alx software enginner

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,9 +13,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 
-	new_node ->str=strdup(str);
-	new_node ->len=strlen(str);
-	new_node ->next=*head;
-	*head =new_node;
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
 	return (new_node);
 }
